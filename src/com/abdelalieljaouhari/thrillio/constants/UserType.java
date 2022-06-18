241d3a533e9cb03c88e04bc4fe06c60f5769c1c0
ElAbdelali <eljaouhariabdelali@outlook.com>
2022-06-18 14:25:02 -0500

    Completed the Constant Classes BookGenre, UserType, Gender and MovieGenre

--- src/com/abdelalieljaouhari/thrillio/constants/UserType.java
+++ src/com/abdelalieljaouhari/thrillio/constants/UserType.java
@@ -2,4 +2,9 @@ package com.abdelalieljaouhari.thrillio.constants;
 
 public class UserType {
 
+	private UserType() {}
+	
+	public static final String USER = "user";
+	public static final String EDITOR = "editor";
+	public static final String CHIEF_EDITOR = "chiefeditor";
 }
